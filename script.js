72585cfbd7e804c4fa1cbebf9ec67cd391bc20bd
Bright Carter <brightonwuemeri@gmail.com>
2023-06-29 20:23:20 +0100

    first javascript project , to do list

---
+++ script.js
@@ -0,0 +1,30 @@
+const inputBox =document.getElementById('input-box');
+
+const listContainer = document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Please write a task in the space provide below");
+    }
+
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value= '';
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
